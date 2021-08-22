ae5478d5f3d2c9c66d146e5bdd2e4e9bfe1a8984
Kyland K <kkainbryce@oh.rr.com>
2021-08-22 13:00:49 -0400

    Sample Rate Change; R:OC, TB
    18500 Sample Rate; TurboBoost+Reverse OverClock Perimeters, to be used in conjunction with earlier Smart FrameSkip Commits

--- src/libretro/libretro.c
+++ src/libretro/libretro.c
@@ -91,8 +91,8 @@ void retro_set_audio_buff_status_cb(void)
 void retro_set_environment(retro_environment_t cb)
 {
    static const struct retro_variable vars[] = {
-      { "mame2003-xtreme-frameskip", "Frameskip; disabled|1|2|3|4|5|6|7|8|9|10|11|auto|auto_aggressive|auto_max" },
-      { "mame2003-xtreme-oc", "cpu clock; 100|25|30|35|40|45|50|55|60|65|70|75|80|85|90|95|105|110|115|120|125" },
+      { "mame2003-xtreme-frameskip", "TurboBoost; disabled|1|2|3|4|5|6|7|8|9|10|11|12|13|14|15|16|17|18|19|20|auto|auto_aggressive|auto_max" },
+      { "mame2003-xtreme-oc", "Reverse OverClock; 100|1|2|3|4|5|6|7|8|9|10|11|12|13|14|15|16|17|18|19|20|21|22|23|24|25|26|27|28|29|30|31|32|33|34|35|36|37|38|39|40|41|42|43|44|45|46|47|48|49|50|51|52|53|54|55|56|57|58|59|60|61|62|63|64|65|66|67|68|69|70|71|72|73|74|75|76|77|78|79|80|81|82|83|84|85|86|87|88|89|90|91|92|93|94|95|96|97|98|99|100|101|102|103|104|105|106|107|108|109|110|111|112|113|114|115|116|117|118|119|120|121|122|123|124|125|126|127|128|129|130|131|132|133|134|135|136|137|138|139|140|141|142|143|144|145|146|147|148|149|150|151|152|153|154|155|156|157|158|159|160|161|162|164|164|165|166|167|168|169|170|171|172|173|174|175|176|177|178|179|180|181|182|183|184|185|186|187|188|189|190|191|192|193|194|195|196|197|198|199|200" },
       { "mame2003-xtreme-dcs-speedhack",
 #if defined(__CELLOS_LV2__) || defined(GEKKO) || defined(_XBOX)
          "MK2/MK3 DCS Speedhack; disabled|enabled"
@@ -103,7 +103,7 @@ void retro_set_environment(retro_environment_t cb)
       { "mame2003-xtreme-skip_disclaimer", "Skip Disclaimer; enabled|disabled" },
       { "mame2003-xtreme-skip_warnings", "Skip Warnings; disabled|enabled" },
       { "mame2003-xtreme-samples", "Samples; enabled|disabled" },
-      { "mame2003-xtreme-sample_rate", "Sample Rate (KHz); 48000|8000|11025|22050|44100" },
+      { "mame2003-xtreme-sample_rate", "Sample Rate (KHz); 48000|8000|11025|18500|22050|44100" },
       { "mame2003-xtreme-cheats", "Cheats; disabled|enabled" },
       { "mame2003-xtreme-dialsharexy", "Share 2 player dial controls across one X/Y device; disabled|enabled" },
 #if defined(__IOS__)
