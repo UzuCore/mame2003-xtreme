030a11f414b9fadf56f3bd4a5e537c2747f630f9
Grantonius maximus <136645825+grant2258@users.noreply.github.com>
2024-08-12 18:20:28 +0100

    trim and edge

--- src/libretro/libretro.c
+++ src/libretro/libretro.c
@@ -1049,7 +1049,7 @@ static void configure_cyclone_mode (int driverIndex)
       }
     }
    if (disable_z80)
-     log_cb(RETRO_LOG_INFO, LOGPRE "z80 disabled due to interrupt issues for now\n"); 
+     log_cb(RETRO_LOG_INFO, LOGPRE "z80 disabled due to interrupt issues for now\n");
   }
 #endif 
 
